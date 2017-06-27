523c4b5018217800eccb82688da5f16ffb790d7d
Meagan Blais <meaganblais@gmail.com>
2017-06-27 17:59:00 -0400

    setting the initial state

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,40 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx'
 import MessageList from './MessageList.jsx'
 
+//setting state to include all props necessary for app
 class App extends Component {
+  constructor(props) {
+    console.log('setting constructor')
+    super(props);
+    this.state = {
+      loading: false,
+      currentUser: null,
+      messages: []
+    };
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount', this.state)
+    setTimeout(() => {
+      this.setState({loading: true})
+    }, 3000)
+  }
+
   render() {
     console.log('App');
-    return (
-      <div>
-        <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty</a>
-      </nav>
-      <MessageList />
-      <ChatBar />
-      </div>
-    );
+    if(this.state.loading) {
+      return (
+        <div>
+          <nav className="navbar">
+            <a href="/" className="navbar-brand">Chatty</a>
+          </nav>
+          <MessageList />
+          <ChatBar />
+        </div>
+      );
+    } else {
+      return <h1>3 seconds have elapsed and page is loading</h1>
+    }
   }
 }
 export default App;
